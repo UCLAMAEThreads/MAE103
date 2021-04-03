ccfb93738b14dbcc3729f2b2ef2e3bfce1d2dc70
Jeff Eldredge <jdeldre@g.ucla.edu>
2021-04-03 15:10:18 -0700

    Fixed up notebook 1.2

--- src/MAE103.jl
+++ src/MAE103.jl
@@ -18,10 +18,12 @@ module MAE103
   ##using PyPlot
   using Conda
 
+  @reexport using ThermofluidQuantities
+
   # This form is necessary to add new units
-  import Unitful
-  using Unitful: @unit, @u_str, Quantity, @derived_dimension, uconvert
-  export @u_str
+  #import Unitful
+  #using Unitful: @unit, @u_str, Quantity, @derived_dimension, uconvert
+  #export @u_str
 
   @reexport using Statistics
 
@@ -35,22 +37,23 @@ module MAE103
   #notebook_dir = joinpath(proj_dir,"notebook")
 
 
-
+  #=
   include("units.jl")
   include("utils.jl")
   include("quantities.jl")
   include("gases.jl")
   include("liquids.jl")
+  =#
 
   include("plot_recipes.jl")
 
 
-  const localunits = Unitful.basefactors
+  #const localunits = Unitful.basefactors
 
   function __init__()
 
-    merge!(Unitful.basefactors, localunits)
-    Unitful.register(MAE103)
+    #merge!(Unitful.basefactors, localunits)
+    #Unitful.register(MAE103)
 
     @require Plots="91a5bcdd-55d7-5caf-9e0b-520d859cae80" begin
 
---
+++ test/literate/0.0-Index.jl
@@ -0,0 +1,46 @@
+include("header.jl")
+
+#=
+These Jupyter notebooks and associated code serve as a companion to the course.
+
+These are not static notebooks. **You are encouraged to play with them: run with different parameter values, create other plots, etc.!** The whole point is to enable an understanding of fluid mechanics through interactiveness.
+
+*The code is released under the [MIT license](https://opensource.org/licenses/MIT).*
+<hr />
+=#
+
+#=
+## Table of Notebooks
+
+- [Introduction](0.1-WelcomeToJulia.ipynb)
+
+#### Chapter 1 - Fluid properties, flow quantities, and dimensional analysis
+
+- [1.0 Units and quantities](1.0-UnitsAndQuantities.ipynb)
+- [1.1 Velocity profiles](1.1-VelocityProfiles.ipynb)
+- [1.2 Dimensionless quantities](1.2-DimensionlessQuantities.ipynb)
+- [1.3 Dimensional analysis](1.3-DimensionalAnalysis.ipynb)
+
+
+#### Chapter 2 - Fluid statics
+
+-
+
+#### Chapter 3 - Flow kinematics
+
+-
+
+#### Chapter 4 - Control volume analysis
+
+-
+
+#### Chapter 5 - Differential analysis and pipe flow
+
+-
+
+#### Chapter 6 - Compressible flow
+
+-
+
+
+=#
